cad71c9dcadceb9dd578dbd3ab74bfd0028b038d
TryLoess <2624680754@qq.com>
2025-05-08 09:31:43 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
 import pandas as pd
-from src.utils import *
+from src.test_utils import *
 from need_zip_dir.Predictor import *
+import json
+
 
 base_dir = get_dir_name(True)
-data = pd.read_csv(base_dir + "/feature_set/no_feature_sym0.csv")
+with open(base_dir + "/need_zip_dir/config.json", "r") as f:
+    config = json.load(f)
+feature = config["feature"]
+data = pd.read_csv(base_dir + "/feature_set/no_feature_sym0.csv")[feature]
 print("初始化完成")
 pre = Predictor()
-pre.predict([data[:5], data[5:10]])
+pre.predict([data.iloc[:100], data.iloc[100:200]])
